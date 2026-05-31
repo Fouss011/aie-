6906c65f8934c810ca7773d5667deb71966f49e0
Fouss011 <foaniss@gmail.com>
2026-05-31 23:18:16 +0200

    Allow Monyva frontend origin in CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,12 +22,13 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost",
-      "https://localhost",
-      "capacitor://localhost",
-      "https://neneye.netlify.app",
-    ],
+  "http://localhost:5173",
+  "http://localhost",
+  "https://localhost",
+  "capacitor://localhost",
+  "https://neneye.netlify.app",
+  "https://monyva.netlify.app",
+],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
